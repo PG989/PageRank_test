bbdd435e5ba98430b7690d7948073b5ce8d7011d
PG989 <paloczig@gmail.com>
2020-09-07 11:09:58 +0200

    Create PageRankGit.R

---
+++ R/PageRankGit.R
@@ -0,0 +1,19 @@
+PageRank <- function(pdb = ""){
+  #install.packages('igraph')
+  #library(igraph)
+  
+  # pdb <- fromJSON(jsonInput)
+  # g <- pdb[, "Package"] %>%
+  #   makeDepGraph(availPkgs = pdb, suggests=FALSE, enhances=TRUE, includeBasePkgs = FALSE)
+  #   
+  #   pr <- g %>%
+  #     page.rank(directed = FALSE) %>%
+  #     use_series("vector") %>%
+  #     sort(decreasing = TRUE) %>%
+  #     as.matrix %>%
+  #     set_colnames("page.rank")
+    
+    
+    message = paste(pdb[1,1])
+    
+}
